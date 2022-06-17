616cf23c02aa13a7885326c2084b622b83e693f2
ghostrush <nicholas.giemsa@Live.com.au>
2022-06-17 11:08:25 +1000

    Added robots.txt check

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 import os
+import requests
+import urllib3
+
+#TEST_URL = "192.168.1.139"
+TEST_URL_ROBOTS = "www.google.com.au"
+
+#Can I get this to return an error if not found
+def curlforbots(url):
+    result = os.popen(f"curl -v https://{url}/robots.txt").read()
+    with open("robots.txt", "w") as f:
+        print(result, file=f)
+    print(result)
+
+
+curlforbots(TEST_URL_ROBOTS)
